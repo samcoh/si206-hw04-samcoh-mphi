327fb7dc8e1be592b5fa223c4224990a357a2e9e
Michelle Phillips <mphi@umich.edu>
2018-01-31 10:22:16 -0500

    adding answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
+from random import randrange
+
 def question():
     question = input("What is your question?")
+
+def add_questions():
+    answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
+    'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Do not count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+    x = randrange(answers)
+    print(answers[x])
