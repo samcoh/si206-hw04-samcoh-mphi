e32c210d94281aed2d937bb0e9cddb3faab3bc5e
Samantha Cohen <Sammie@0587344525.wireless.umich.net>
2018-01-31 10:23:15 -0500

    Added checks_question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
-def question():
-    question = input("What is your question?")
+user = input("What is your question?")
+while user != "quit":
+    def question():
+        question = input("What is your question?")
+        return question
+    def checks_question(user):
+        if "?" not in user:
+            print("I’m sorry, I can only answer questions.")
+    checks_question(user)
+    user = question()
